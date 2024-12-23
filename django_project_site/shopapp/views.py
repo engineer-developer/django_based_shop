0b5aee423c7c03495c249ef4b2078c1b81693eae
Alex Suslov <suslov.lexx@gmail.com>
2024-12-24 00:14:17 +0500

    hw_3.6 complete

--- django_project_site/shopapp/views.py
+++ django_project_site/shopapp/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.contrib.auth.models import Group
 from timeit import default_timer
 from datetime import datetime
+
+from shopapp.models import Product, Order
 
 
 def show_greetings(request: HttpRequest):
@@ -28,4 +31,37 @@ def show_index(request: HttpRequest):
         request,
         "shopapp/shop-index.html",
         context=context,
+    )
+
+
+def groups_list(request: HttpRequest):
+    context = {"groups": Group.objects.prefetch_related("permissions").all()}
+    return render(
+        request,
+        "shopapp/groups-list.html",
+        context=context,
+    )
+
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(
+        request,
+        "shopapp/products-list.html",
+        context,
+    )
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user")
+        .prefetch_related("products")
+        .all(),
+    }
+    return render(
+        request,
+        "shopapp/orders-list.html",
+        context,
     )
