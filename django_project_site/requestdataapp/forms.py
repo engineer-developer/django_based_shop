0641da85446c3fba12456baa6765e4bc3fbfa139
Alex Suslov <suslov.lexx@gmail.com>
2024-12-29 21:15:55 +0500

    edit UserBioForm

--- django_project_site/requestdataapp/forms.py
+++ django_project_site/requestdataapp/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class UserBioForm(forms.Form):
     name = forms.CharField(max_length=100, label="Full name")
-    age = forms.IntegerField(label="Your age")
+    age = forms.IntegerField(label="Your age", min_value=1, max_value=120)
     bio = forms.CharField(label="Biography", widget=forms.Textarea)
