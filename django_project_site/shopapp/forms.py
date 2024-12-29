8df164e67352c75f1ff802e980aa99e6d63383aa
Alex Suslov <suslov.lexx@gmail.com>
2024-12-29 22:18:11 +0500

    add validators to ProductForm

--- django_project_site/shopapp/forms.py
+++ django_project_site/shopapp/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from django.core import validators
 
 
 class ProductForm(forms.Form):
     name = forms.CharField(max_length=100)
     price = forms.DecimalField(min_value=1, max_value=100000, decimal_places=2)
-    description = forms.CharField(label="Product description", widget=forms.Textarea)
+    description = forms.CharField(
+        label="Product description",
+        widget=forms.Textarea(attrs={"rows": 5, "cols": 30}),
+        validators=[
+            validators.RegexValidator(
+                regex=r"great",
+                message="Product description must contain word 'great'",
+            )
+        ],
+    )
