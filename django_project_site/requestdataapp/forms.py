4cc8811b0576f7300aba6260ed349260ba0fc364
Alex Suslov <suslov.lexx@gmail.com>
2024-12-29 22:18:40 +0500

    add UploadFileForm

--- django_project_site/requestdataapp/forms.py
+++ django_project_site/requestdataapp/forms.py
@@ -5,3 +5,7 @@ class UserBioForm(forms.Form):
     name = forms.CharField(max_length=100, label="Full name")
     age = forms.IntegerField(label="Your age", min_value=1, max_value=120)
     bio = forms.CharField(label="Biography", widget=forms.Textarea)
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
