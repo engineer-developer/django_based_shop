9a90b239f5aba9d35344254354e38d381f419ef7
Alex Suslov <suslov.lexx@gmail.com>
2024-12-29 22:38:14 +0500

    edit ProductForm: change to forms.ModelForm

--- django_project_site/shopapp/forms.py
+++ django_project_site/shopapp/forms.py
@@ -2,16 +2,32 @@ from django import forms
 from django.core import validators
 
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    price = forms.DecimalField(min_value=1, max_value=100000, decimal_places=2)
-    description = forms.CharField(
-        label="Product description",
-        widget=forms.Textarea(attrs={"rows": 5, "cols": 30}),
-        validators=[
-            validators.RegexValidator(
-                regex=r"great",
-                message="Product description must contain word 'great'",
-            )
-        ],
-    )
+from shopapp.models import Product
+
+
+# Implemented forms.Form
+# class ProductForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     price = forms.DecimalField(min_value=1, max_value=100000, decimal_places=2)
+#     description = forms.CharField(
+#         label="Product description",
+#         widget=forms.Textarea(attrs={"rows": 5, "cols": 30}),
+#         validators=[
+#             validators.RegexValidator(
+#                 regex=r"great",
+#                 message="Product description must contain word 'great'",
+#             )
+#         ],
+#     )
+
+
+# Implemented forms.ModelForm
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "price",
+            "description",
+            "discount",
+        ]
