ec0ec42f970b5580c39821fd91042ab238ea85a6
Alex Suslov <suslov.lexx@gmail.com>
2024-12-29 22:19:09 +0500

    edit handle_file_upload

--- django_project_site/requestdataapp/views.py
+++ django_project_site/requestdataapp/views.py
@@ -2,7 +2,7 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
-from requestdataapp.forms import UserBioForm
+from requestdataapp.forms import UserBioForm, UploadFileForm
 
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
@@ -37,15 +37,23 @@ def handle_file_upload(
     request: HttpRequest,
     max_size: int = FILE_MAX_SIZE,
 ) -> HttpResponse:
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        if myfile.size > max_size:
-            return HttpResponse("File too large", status=400)
-        filename = fs.save(myfile.name, myfile)
-        print("saved file", filename)
 
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            if myfile.size > max_size:
+                return HttpResponse("File too large", status=400)
+            filename = fs.save(myfile.name, myfile)
+            print("saved file", filename)
+    else:
+        form = UploadFileForm()
+
+    context = {"form": form}
     return render(
         request,
         "requestdataapp/file-upload.html",
+        context=context,
     )
