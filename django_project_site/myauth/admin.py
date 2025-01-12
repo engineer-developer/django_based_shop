fde76f781e697ff92f5266c6055a67e161779bac
Alex Suslov <suslov.lexx@gmail.com>
2025-01-12 21:02:16 +0500

    add ProductsInline to UserAdmin

--- django_project_site/myauth/admin.py
+++ django_project_site/myauth/admin.py
@@ -3,14 +3,23 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from myauth.models import Profile
+from shopapp.models import Product
 
 
 class ProfileInline(admin.StackedInline):
     model = Profile
 
 
+class ProductsInline(admin.StackedInline):
+    model = Product
+    extra = 0
+
+
 class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline,)
+    inlines = (
+        ProfileInline,
+        ProductsInline,
+    )
 
 
 admin.site.unregister(User)
