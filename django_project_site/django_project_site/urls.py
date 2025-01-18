8082e9a60603bf95fcbd5847847c868bbc20fa68
Alex Suslov <suslov.lexx@gmail.com>
2025-01-18 23:15:03 +0500

    add static to urlpatterns

--- django_project_site/django_project_site/urls.py
+++ django_project_site/django_project_site/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -30,3 +32,8 @@ urlpatterns = [
         RedirectView.as_view(url="/static/shopapp/favicon.ico", permanent=True),
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    )
