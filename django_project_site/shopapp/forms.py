2efc2fb1744d7b19ad8f4fd98c236374d4b63adf
Alex Suslov <suslov.lexx@gmail.com>
2025-01-19 13:29:14 +0500

    create MultipleImageField

--- django_project_site/shopapp/forms.py
+++ django_project_site/shopapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core import validators
 from django.contrib.auth.models import User, Group
+from django.forms import ClearableFileInput
 
 from shopapp.models import Product, Order
 
@@ -23,6 +24,20 @@ from shopapp.models import Product, Order
 
 class MultipleClearableFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
+
+
+class MultipleImageField(forms.ImageField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleClearableFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
 
 
 # Implemented forms.ModelForm
