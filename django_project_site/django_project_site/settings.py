88fa2646811776a08ca4a17204c4aaf8188062c4
Alex Suslov <suslov.lexx@gmail.com>
2025-01-20 22:44:41 +0500

    add to settings MIDDLEWARE: "django.middleware.locale.LocaleMiddleware"

--- django_project_site/django_project_site/settings.py
+++ django_project_site/django_project_site/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     # "requestdataapp.middlewares.set_useragent_on_request_middleware",
     # "requestdataapp.middlewares.CountRequestsMiddleware",
     # "requestdataapp.middlewares.ThrottlingMiddleware",
