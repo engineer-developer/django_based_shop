7efb99af361034d241a22126b3ce40ff351a28f1
Alex Suslov <suslov.lexx@gmail.com>
2025-01-26 17:21:33 +0500

    add verbose_name and verbose_name_plural to Order

--- django_project_site/shopapp/models.py
+++ django_project_site/shopapp/models.py
@@ -61,6 +61,8 @@ class ProductImage(models.Model):
 class Order(models.Model):
     class Meta:
         ordering = ["created_at"]
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
 
     delivery_address = models.TextField(null=True, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
