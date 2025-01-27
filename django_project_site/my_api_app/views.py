2f3fbfad53b541282fbab0c978cf372db1b69324
Alex Suslov <suslov.lexx@gmail.com>
2025-01-27 21:30:05 +0500

    rebase GroupsListView with (ListCreateAPIView)

--- django_project_site/my_api_app/views.py
+++ django_project_site/my_api_app/views.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import Group
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from django.contrib.auth.models import Group
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.mixins import ListModelMixin
 
 from .serializers import GroupSerializer
 
@@ -31,3 +33,8 @@ def hello_world_view(request: Request) -> Response:
 #     def get(self, request: Request) -> Response:
 #         return self.list(request)
 
+
+# Implement "ListCreateAPIView"
+class GroupsListView(ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
