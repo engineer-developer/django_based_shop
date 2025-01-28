4860e156892bb5ab14075d91c1e299a6b8e53813
Alex Suslov <suslov.lexx@gmail.com>
2025-01-28 21:26:04 +0500

    setup REST_FRAMEWORK: add DEFAULT_SCHEMA_CLASS

--- django_project_site/django_project_site/settings.py
+++ django_project_site/django_project_site/settings.py
@@ -163,4 +163,5 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
