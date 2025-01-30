ed3328892cac1c81280917ecd5b459becbef66de
Alex Suslov <suslov.lexx@gmail.com>
2025-01-31 00:21:58 +0500

    add admin models

--- django_project_site/blogapp/admin.py
+++ django_project_site/blogapp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogapp.models import Article, Author, Tag, Category
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "bio")
+    list_display_links = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display_links = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display_links = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "content",
+        "pub_date",
+        "author",
+        "category",
+    )
+    search_fields = ("title", "content", "category", "tags")
