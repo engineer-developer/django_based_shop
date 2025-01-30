ed423f5a74d482d6a5e758d9599a79c580fb4ca9
Alex Suslov <suslov.lexx@gmail.com>
2025-01-30 21:33:46 +0500

    create models Author, Category, Tag

--- django_project_site/blogapp/models.py
+++ django_project_site/blogapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"<Author {self.name}> pk={self.pk}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"<Category {self.name}> pk={self.pk}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"<Tag {self.name}> pk={self.pk}"
