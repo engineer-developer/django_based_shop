2ad90e585c0fa5300edaedc5e2cc7cab6b475263
Alex Suslov <suslov.lexx@gmail.com>
2025-01-30 21:53:59 +0500

    create and add to db model Author

--- django_project_site/blogapp/models.py
+++ django_project_site/blogapp/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.TextField()
+    bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"<Author {self.name}> pk={self.pk}"
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-
-    def __str__(self):
-        return f"<Category {self.name}> pk={self.pk}"
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f"<Tag {self.name}> pk={self.pk}"
+# class Category(models.Model):
+#     name = models.CharField(max_length=40)
+#
+#     def __str__(self):
+#         return f"<Category {self.name}> pk={self.pk}"
+#
+#
+# class Tag(models.Model):
+#     name = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return f"<Tag {self.name}> pk={self.pk}"
