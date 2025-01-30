80944e7f9576033eb2c1b207626734bae70514f1
Alex Suslov <suslov.lexx@gmail.com>
2025-01-30 22:19:01 +0500

    add to views.py ArticleListView

--- django_project_site/blogapp/views.py
+++ django_project_site/blogapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from blogapp.models import Article
+
+
+class ArticleListView(ListView):
+    """List of all articles"""
+
+    queryset = (
+        Article.objects.select_related("author")
+        .select_related("category")
+        .prefetch_related("tags")
+        .order_by("-pub_date")
+    )
+    template_name = "blogapp/article_list.html"
+    context_object_name = "articles"
