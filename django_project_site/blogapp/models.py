87e2d5fb3d6f22f99a33f2ce632c8cb309cc4226
Alex Suslov <suslov.lexx@gmail.com>
2025-01-30 21:55:17 +0500

    create and add to db model Tag

--- django_project_site/blogapp/models.py
+++ django_project_site/blogapp/models.py
@@ -16,8 +16,8 @@ class Category(models.Model):
         return f"<Category {self.name}> pk={self.pk}"
 
 
-# class Tag(models.Model):
-#     name = models.CharField(max_length=20)
-#
-#     def __str__(self):
-#         return f"<Tag {self.name}> pk={self.pk}"
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"<Tag {self.name}> pk={self.pk}"
