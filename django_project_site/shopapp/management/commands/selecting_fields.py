fc4c310b4fb9186f9b4905ba10fbfc3618e95a6a
Alex Suslov <suslov.lexx@gmail.com>
2025-01-30 18:49:39 +0500

    upd selecting_fields.py: uses values_list

--- django_project_site/shopapp/management/commands/selecting_fields.py
+++ django_project_site/shopapp/management/commands/selecting_fields.py
@@ -12,8 +12,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Start demo select fields")
 
-        products_values = Product.objects.values("pk", "name")
-        for p_values in products_values:
-            print(p_values)
+        # products_values = Product.objects.values("pk", "name")
+        # for p_values in products_values:
+        #     print(p_values)
 
-        self.stdout.write(f"Done")
+        users_info = User.objects.values_list("pk", "username")
+        for user_info in users_info:
+            print(user_info)
+
+        self.stdout.write(self.style.SUCCESS("Done"))
