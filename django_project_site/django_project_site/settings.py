3369a095060239be3af4e6d479569123586f3796
Alex Suslov <suslov.lexx@gmail.com>
2025-01-31 20:08:18 +0500

    comment Logging settings for view SQL-QUERIES in console

--- django_project_site/django_project_site/settings.py
+++ django_project_site/django_project_site/settings.py
@@ -176,6 +176,28 @@ SPECTACULAR_SETTINGS = {
 }
 
 # Logging rule
+# Logging settings for view SQL-QUERIES in console
+# LOGGING = {
+#     "version": 1,
+#     "filters": {
+#         "require_debug_true": {
+#             "()": "django.utils.log.RequireDebugTrue",
+#         }
+#     },
+#     "handlers": {
+#         "console": {
+#             "level": "DEBUG",
+#             "filters": ["require_debug_true"],
+#             "class": "logging.StreamHandler",
+#         }
+#     },
+#     "loggers": {
+#         "django.db.backends": {
+#             "level": "DEBUG",
+#             "handlers": ["console"],
+#         }
+#     },
+# }
 LOGGING = {
     "version": 1,
     "filters": {
