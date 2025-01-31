2ed91967ac0aba07bc7f6eead297e40aa952944b
Alex Suslov <suslov.lexx@gmail.com>
2025-01-31 20:37:18 +0500

    add formatters to logging

--- django_project_site/django_project_site/settings.py
+++ django_project_site/django_project_site/settings.py
@@ -202,10 +202,14 @@ SPECTACULAR_SETTINGS = {
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "%(asctime)s - [%(levelname)s] - %(name)s: %(message)s"},
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
         }
+            "formatter": "verbose",
     },
     "root": {
         "handlers": ["console"],
