ea78739216bd1d3fb06155c9b3e2cb09922075bf
Alex Suslov <suslov.lexx@gmail.com>
2025-02-05 21:40:21 +0500

    create function save_csv_orders

--- django_project_site/shopapp/common.py
+++ django_project_site/shopapp/common.py
@@ -1,7 +1,9 @@
 from csv import DictReader
 from io import TextIOWrapper
 
-from shopapp.models import Product
+from django.contrib.auth.models import User
+
+from shopapp.models import Product, Order
 
 
 def save_csv_products(file, encoding):
@@ -13,3 +15,26 @@ def save_csv_products(file, encoding):
     products = [Product(**row) for row in reader]
     Product.objects.bulk_create(products)
     return products
+
+
+def save_csv_orders(file, encoding):
+    csv_file = TextIOWrapper(
+        buffer=file,
+        encoding=encoding,
+    )
+    reader = DictReader(csv_file)
+
+    new_orders = []
+    for row in reader:
+        order = Order(
+            delivery_address=row["delivery_address"],
+            promocode=row["promocode"],
+            user=User.objects.get(pk=row["user"]),
+        )
+        order.save()
+        products_ids = row["products"].split(" ")
+        order_products = Product.objects.filter(pk__in=products_ids)
+        order.products.set(order_products)
+        new_orders.append(order)
+
+    return new_orders
