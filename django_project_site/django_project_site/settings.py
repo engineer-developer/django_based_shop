a9ea09014b43991d76416579c42fe57bd435305a
Alex Suslov <suslov.lexx@gmail.com>
2025-02-11 23:41:40 +0500

    upd settings.py: add STATIC_ROOT

--- django_project_site/django_project_site/settings.py
+++ django_project_site/django_project_site/settings.py
@@ -182,6 +182,8 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT.mkdir(exist_ok=True)
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
