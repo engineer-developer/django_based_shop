6f7adaaa2eb1984afd445ac10e2f5cfdcfc18af0
Alex Suslov <suslov.lexx@gmail.com>
2025-02-15 04:47:56 +0500

    add verbose_name

--- django_project_site/shopapp/apps.py
+++ django_project_site/shopapp/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ShopappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shopapp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shopapp"
+    verbose_name = "Магазин"
